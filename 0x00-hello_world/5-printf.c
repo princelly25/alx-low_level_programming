4f7bbf42b60df388e7c23ad65f27eaff664a33af
princelly25 <princeeffiong20@gmail.com>
2022-12-12 05:32:20 -0800

    using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - print a c program using printf
+ *
+ * Return: 0
+ */
+
 int main(void)
+
 {
-	/* printf function*/
+
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
