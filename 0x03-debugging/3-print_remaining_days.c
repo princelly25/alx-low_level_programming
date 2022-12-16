af252d0b64e50e0d70353478d9e5b98b802c6f0d
princelly25 <princeeffiong20@gmail.com>
2022-12-15 21:06:38 -0800

    task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#include <main.h>
+#include "main.h"
 
 
 
@@ -24,7 +24,7 @@
 void print_remaining_days(int month, int day, int year)
 
 {
-	if ((year % 400 == 0 || year % 4 == 0) && !(year % 100 != 0))
+	if ((year % 400 == 0) || (year % 4 == 0) && !(year % 100 != 0))
 	{
 			if (month >= 2 && day >= 60)
 
